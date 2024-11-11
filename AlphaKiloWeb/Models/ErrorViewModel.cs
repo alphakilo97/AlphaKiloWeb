18b8a835a436419f94da2a4beab0f962e59d0057
alphakilo97 <avraam14@outlook.com>
2024-11-11 16:37:47 +0200

    Add project files.

---
+++ AlphaKiloWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace AlphaKiloWeb.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
