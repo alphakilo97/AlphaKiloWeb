8da59e3e5871e10efa94fbd7097f06ff509872ec
alphakilo97 <avraam14@outlook.com>
2024-11-12 17:58:38 +0200

    Added Create operation for Category with views and validations

--- AlphaKiloWeb/Controllers/CategoryController.cs
+++ AlphaKiloWeb/Controllers/CategoryController.cs
@@ -12,5 +12,20 @@ namespace AlphaKiloWeb.Controllers {
             List<Category> categoryList = _db.Categories.ToList();
             return View(categoryList);
         }
+        public IActionResult Create() {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category newCategory) {
+            if(double.TryParse(newCategory.Name, out double i)){
+                ModelState.AddModelError("name", "Category Name cannot be numeric");
+            }
+            if(ModelState.IsValid){
+                _db.Categories.Add(newCategory);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
--- AlphaKiloWeb/Models/Category.cs
+++ AlphaKiloWeb/Models/Category.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlphaKiloWeb.Models {
     public class Category {
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(20)]
         public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public int DisplayOrder { get; set; }
     }
 }
