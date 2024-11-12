5f3b9ac079a422f90ad1d1fdae78b31557b75bfb
alphakilo97 <avraam14@outlook.com>
2024-11-12 13:02:36 +0200

    Configured DB, created Categories Controller, View and database migration.

---
+++ AlphaKiloWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AlphaKiloWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlphaKiloWeb.Data {
+    public class ApplicationDbContext : DbContext{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
