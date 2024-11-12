45ebef913261ea63f6c43ed78240f15913620abe
alphakilo97 <avraam14@outlook.com>
2024-11-12 16:32:18 +0200

    Added Seeder for Categories, Added Bootstrap

--- AlphaKiloWeb/Controllers/CategoryController.cs
+++ AlphaKiloWeb/Controllers/CategoryController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AlphaKiloWeb.Data;
+using AlphaKiloWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AlphaKiloWeb.Controllers {
     public class CategoryController : Controller {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
         public IActionResult Index() {
-            return View();
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
--- AlphaKiloWeb/Migrations/20241112111018_SeedCategoriesTable.Designer.cs
+++ AlphaKiloWeb/Migrations/20241112111018_SeedCategoriesTable.Designer.cs
@@ -3,6 +3,7 @@ using AlphaKiloWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AlphaKiloWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241112111018_SeedCategoriesTable")]
+    partial class SeedCategoriesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
