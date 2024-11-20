87ee48ea21dc797868b8f8dad0ac5614df00c8c4
alphakilo97 <avraam14@outlook.com>
2024-11-20 14:34:12 +0200

    Added .NET Identity. Extended IdentityUser into Application user. Added migrations.

--- AlphaKilo.DataAccess/Data/ApplicationDbContext.cs
+++ AlphaKilo.DataAccess/Data/ApplicationDbContext.cs
@@ -1,15 +1,20 @@
 ﻿using AlphaKilo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace AlphaKilo.DataAccess.Data {
-    public class ApplicationDbContext : DbContext{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>{
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
             
         }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers {  get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 2 },
                 new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 4 },
@@ -76,6 +81,7 @@ namespace AlphaKilo.DataAccess.Data {
                     CategoryId = 4
                 }
             );
+
         }
 
     }
--- AlphaKiloWeb/Program.cs
+++ AlphaKiloWeb/Program.cs
@@ -3,6 +3,7 @@ using AlphaKilo.DataAccess.Repository;
 using AlphaKilo.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using System.Globalization;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(/*options => options.SignIn.RequireConfirmedAccount = true*/).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var defaultCulture = new CultureInfo("en-FR");
@@ -29,9 +33,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
